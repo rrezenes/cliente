8b2e0a017ce5614dab641a9cfcaa3f9c0193001c
rrezenes <rezeness@gmail.com>
2018-04-19 22:07:23 -0300

    Finalizado metodo testCreateUser()

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+use cliente\User;
+
+class UserTest extends TestCase
+{
+	//Faz todo o processo e depois da um rollback na base de dados
+	use DatabaseTransactions; 
+
+    public function testCreateUser()
+    {
+
+		$dados = [
+			'name' => 'Teste',
+			'email' => 'teste03@email.com',
+			'password' => 'teste',
+			'active' => '1'
+		];
+
+		$response = $this->json('POST', 'api/users', $dados);
+
+		$response
+			->assertStatus(201)
+			->assertJsonStructure(
+    			[
+    				'id',
+        			'name',
+        			'email',
+        			'active'
+				]
+			);
+    }
+
+
+}
